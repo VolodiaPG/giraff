f2d538a448775a4307034c4ba000dc150042eec9
VolodiaPG <volodia.parol-guarino@protonmail.com>
2022-03-03 11:09:50 +0100

    Fix rust-warp openfass template to use cache, but not ignore new changes

--- scripts/all.sh
+++ scripts/all.sh
@@ -3,8 +3,8 @@ total=${#files[@]}
 i=0
 for file in "${files[@]}"; do
     i=$((i+1))
-    faas-cli up -f $file;
 
-    # Try to display the notification, do not fails if not available
-    notify-send FaaS "[$i/$total] done $file" || true
+    faas-cli up -f $file \
+        && notify-send FaaS "[$i/$total] done $file" \
+        || notify-send FaaS "[$i/$total] failed $file"
 done
