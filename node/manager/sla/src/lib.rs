ad773e5b08913b688a17d36999880be566e95709
VolodiaPG <volodia.parol-guarino@protonmail.com>
2022-03-07 15:57:24 +0100

    Make all SLA fields mandatory

--- node/manager/sla/src/lib.rs
+++ node/manager/sla/src/lib.rs
@@ -1,26 +1,29 @@
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Default)]
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Validate, Serialize, Deserialize, Clone, Debug, PartialEq, Default)]
 pub struct SLA {
     #[serde(rename = "StorageBytes")]
-    pub storage_bytes: Option<u64>,
+    pub storage_bytes: u64,
     
     #[serde(rename = "RAMBytes")]
-    pub ram_bytes: Option<u64>,
+    pub ram_bytes: u64,
     
-    #[validate(min=0, max=1)]
+    #[validate(range(min=0, max=1))]
     #[serde(rename = "CPUFractionPerSeconds")]
-    pub cpu_fraction_per_seconds: Option<f32>,
+    pub cpu_fraction_per_seconds: f32,
 
     #[serde(rename = "LatencyMaxMilliseconds")]
-    pub latency_max_ms: Option<u64>,
+    pub latency_max_ms: u64,
 
     #[serde(rename = "DataInputMaxBytes")]
-    pub data_input_max_bytes: Option<u64>,
+    pub data_input_max_bytes: u64,
 
     #[serde(rename = "DataOutputMaxBytes")]
-    pub data_output_max_bytes: Option<u64>,
+    pub data_output_max_bytes: u64,
 
     #[serde(rename = "MaxMillisecondsBeforeHot")]
-    pub max_milliseconds_before_hot: Option<u64>,
+    pub max_milliseconds_before_hot: u64,
 
     #[serde(rename = "ReevaluationPeriodSeconds")]
     pub reevaluation_period_seconds: Option<u64>,
