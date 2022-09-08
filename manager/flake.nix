73f1981bc98e2a8e61f61b8c97bdb8b86111867b
VolodiaPG <volodia.parol-guarino@proton.me>
2022-09-08 15:20:36 +0200

    WiP

--- manager/flake.nix
+++ manager/flake.nix
@@ -19,6 +19,15 @@
           target = "x86_64-unknown-linux-musl";
           packageFun = import ./Cargo.nix;
         };
+
+        workspaceShell = (rustPkgs.workspaceShell {
+          packages = [
+            pkgs.k3s
+            pkgs.faas-cli
+          ];
+        }); # supports override & overrideAttrs
+
+
         fog_node_bin = (rustPkgs.workspace.fog_node {}).bin;
         market_bin = (rustPkgs.workspace.market {}).bin;
 
@@ -35,6 +44,15 @@
           config.Cmd = [ "${market_bin}/bin/market" ];
         };
 
+        # This is required so that pod can reach the API server (running on port 6443 by default)
+        networking.firewall.allowedTCPPorts = [ 6443 ];
+        services.k3s.enable = true;
+        services.k3s.role = "server";
+        services.k3s.extraFlags = toString [
+          # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+        ];
+        environment.systemPackages = [ pkgs.k3s ];
+
       in rec {
         packages = {
           # replace hello-world with your package name
@@ -45,10 +63,9 @@
 
           default = packages.fog_node;
         };
-        #   devShell = pkgs.mkShell {
-        #   buildInputs =
-        #     [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-        # };
+        devShells = pkgs.mkShell {
+          default = workspaceShell;
+        };
       }
     );
 }
