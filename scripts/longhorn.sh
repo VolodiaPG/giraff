43f4da8021f50dd61025fad8863b4eaac4aff76a
Volodia PAROL-GUARINO <volodia.parol-guarino@proton.me>
2023-01-12 13:54:56 +0100

    chore: Remove deprecated and old stuff

--- scripts/longhorn.sh
+++
@@ -1,9 +0,0 @@
-# Installs Longhorn
-rm -rf /tmp/longhorn
-
-git clone https://github.com/longhorn/longhorn.git --depth 1 /tmp/longhorn
-kubectl create namespace longhorn-system
-helm install longhorn /tmp/longhorn/chart/ --namespace longhorn-system --kubeconfig $HOME/.kube/config --set defaultSettings.defaultDataPath="/storage"
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-
-rm -rf /tmp/longhorn
