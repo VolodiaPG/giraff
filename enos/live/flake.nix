2d1634c8b415aef67e990c6299b4b52613a94237
Volodia PAROL-GUARINO <volodia.parol-guarino@proton.me>
2023-02-27 15:28:46 +0100

    feat: Add support for pre commits checks

--- enos/live/flake.nix
+++ enos/live/flake.nix
@@ -1,78 +1,86 @@
 {
   description = "Application packaged using poetry2nix";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv mkPoetryApplication;
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv;
 
-        overlay = self: super:{
+        overlay = self: super: {
           experiments = self.poetry2nix.mkPoetryEnv {
             projectDir = ./.;
             python = self.python311;
-            overrides = self.poetry2nix.overrides.withDefaults (newattr: oldattr: {
-                cryptography = oldattr.cryptography.overridePythonAttrs (
-                    old: {
-                      cargoDeps =
-                        super.rustPlatform.fetchCargoTarball {
-                          src = old.src;
-                          sourceRoot = "${old.pname}-${old.version}/src/rust";
-                          name = "${old.pname}-${old.version}";
-                          sha256 = "sha256-0x+KIqJznDEyIUqVuYfIESKmHBWfzirPeX2R/cWlngc=";
-                        };
-                    }
-                  );
-                rfc3986-validator = oldattr.rfc3986-validator.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.setuptools oldattr.setuptools-scm oldattr.pytest-runner ];
-                    }
-                  );
-                pathspec = oldattr.pathspec.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.flit-scm oldattr.pytest-runner ];
-                    }
-                  );
-                ncclient = oldattr.ncclient.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.six ];
-                    }
-                  );
-                jupyter-server-terminals = oldattr.jupyter-server-terminals.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatchling ];
-                    }
-                  );
-                jupyter-events = oldattr.jupyter-events.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatchling ];
-                    }
-                  );
-                jupyter-server = oldattr.jupyter-server.overridePythonAttrs
-                  (
-                    old: {
-                      buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatch-jupyter-builder oldattr.hatchling ];
-                    }
-                  );
-              });
+            overrides = self.poetry2nix.overrides.withDefaults (_newattr: oldattr: {
+              cryptography = oldattr.cryptography.overridePythonAttrs (
+                old: {
+                  cargoDeps =
+                    super.rustPlatform.fetchCargoTarball {
+                      src = old.src;
+                      sourceRoot = "${old.pname}-${old.version}/src/rust";
+                      name = "${old.pname}-${old.version}";
+                      sha256 = "sha256-0x+KIqJznDEyIUqVuYfIESKmHBWfzirPeX2R/cWlngc=";
+                    };
+                }
+              );
+              rfc3986-validator = oldattr.rfc3986-validator.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.setuptools oldattr.setuptools-scm oldattr.pytest-runner ];
+                  }
+                );
+              pathspec = oldattr.pathspec.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.flit-scm oldattr.pytest-runner ];
+                  }
+                );
+              ncclient = oldattr.ncclient.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.six ];
+                  }
+                );
+              jupyter-server-terminals = oldattr.jupyter-server-terminals.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatchling ];
+                  }
+                );
+              jupyter-events = oldattr.jupyter-events.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatchling ];
+                  }
+                );
+              jupyter-server = oldattr.jupyter-server.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ oldattr.hatch-jupyter-builder oldattr.hatchling ];
+                  }
+                );
+            });
           };
         };
 
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [overlay];
+          overlays = [ overlay ];
         };
         lib = nixpkgs.lib;
 
@@ -81,41 +89,41 @@
           tag = "latest";
           copyToRoot = pkgs.buildEnv {
             name = "image-root";
-                pathsToLink = [
-                    "/bin"
-                    "/"
-                ];
-                paths = with pkgs; [
-                    # Linux toolset
-                    coreutils
-                    gnused
-                    bashInteractive
+            pathsToLink = [
+              "/bin"
+              "/"
+            ];
+            paths = with pkgs; [
+              # Linux toolset
+              coreutils
+              gnused
+              bashInteractive
 
-                    # My toolset
-                    just
-                    jq
-                    openssh
-                    curl
-                    frp
+              # My toolset
+              just
+              jq
+              openssh
+              curl
+              frp
 
-                    # Environment to run enos and stuff
-                    experiments
-                ];
-            };
-             runAsRoot = ''
-              #!${pkgs.runtimeShell}
-              ${pkgs.dockerTools.shadowSetup}
-              groupadd -g 1000 enos
-              useradd -u 1000 -g 1000 enos
-              mkdir -p /home/enos
-              chown enos:enos -R /home/enos
+              # Environment to run enos and stuff
+              experiments
+            ];
+          };
+          runAsRoot = ''
+            #!${pkgs.runtimeShell}
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd -g 1000 enos
+            useradd -u 1000 -g 1000 enos
+            mkdir -p /home/enos
+            chown enos:enos -R /home/enos
 
-              mkdir /tmp
-              mkdir -p /usr/bin
-              ln -s ${pkgs.coreutils}/bin/env /usr/bin/env
-            '';
+            mkdir /tmp
+            mkdir -p /usr/bin
+            ln -s ${pkgs.coreutils}/bin/env /usr/bin/env
+          '';
           config = {
-            Env = [ "RUN=python" "HOME=/root"]; 
+            Env = [ "RUN=python" "HOME=/root" ];
           };
         };
       in
@@ -125,8 +133,25 @@
           default = pkgs.experiments;
         };
 
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              # Nix
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
+              deadnix.enable = true;
+              # Rust
+              autoflake.enable = true;
+              isort.enable = true;
+              ruff.enable = true;
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ 
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          packages = with pkgs; [
             just
             jq
             poetry2nix.packages.${system}.poetry
