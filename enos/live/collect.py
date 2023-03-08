11eadec856a63d30fb8fe25b9e74659b4ff9d66e
Volodia PAROL-GUARINO <volodia.parol-guarino@proton.me>
2023-03-08 15:22:21 +0100

    feat(experiment): Add data collecting in parrallel an much more space optimized

--- enos/live/collect.py
+++ enos/live/collect.py
@@ -1,25 +1,93 @@
-import codecs
 import csv
 import sys
 import tarfile
-from io import BytesIO
+from io import TextIOWrapper
 from datetime import datetime
 import os
+import tempfile
+import time
 from integration import aliases
-
+import multiprocessing as mp
 import requests
 
+
+class NonClosingSpooledTemporaryFile(tempfile.SpooledTemporaryFile):
+    def close(self):
+        pass
+
+    def free_up(self):
+        tempfile.SpooledTemporaryFile.close(self)
+
+
+def worker(queue, url, metrixName):
+    tmpfile = NonClosingSpooledTemporaryFile(100 * 1024 * 1024)  # 100MiB
+    with TextIOWrapper(tmpfile, encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter="\t")
+        response = requests.get(
+            "{0}/api/v1/query".format(url),
+            params={"query": metrixName + f'[{os.environ["period"]}]'},
+        )
+        results = response.json()["data"]["result"]
+        # Build a list of all labelnames used.
+        # gets all keys and discard __name__
+        labelnames = set()
+        for result in results:
+            labelnames.update(result["metric"].keys())
+        # Canonicalize
+        labelnames.discard("__name__")
+        labelnames = sorted(labelnames)
+
+        writer.writerow(["name"] + labelnames + ["timestamp", "value"])
+        for result in results:
+            for label in labelnames:
+                ll = list(result["metric"].values())
+                for value in result["values"]:
+                    writer.writerow(ll + value)
+
+    queue.put((metrixName, tmpfile))
+
+
+def names(queue):
+    names = aliases()
+    tmpfile = NonClosingSpooledTemporaryFile(100 * 1024 * 1024)  # 100MiB
+    with TextIOWrapper(tmpfile, encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter="\t")
+        writer.writerow(["instance", "name"])
+        for (key, value) in names.items():
+            writer.writerow([key, value])
+    queue.put(("names", tmpfile))
+
+
+def listener(queue, filepath):
+    with tarfile.open(filepath, "w:xz", preset=9) as tar:
+        while True:
+            m = queue.get()
+            if m == "kill":
+                print("killing.")
+                break
+
+            metrixName, tmpfile = m
+
+            tarinfo = tarfile.TarInfo(f"{metrixName}.csv")
+            tarinfo.size = tmpfile.tell()
+            tarinfo.mtime = time.time()
+            tmpfile.seek(os.SEEK_SET)
+            tar.addfile(tarinfo, tmpfile)
+
+            tmpfile.free_up()
+            print(metrixName)
+
+
 if __name__ == "__main__":
+    """
+    Prometheus hourly data as csv.
+    """
     URL = f"http://localhost:{os.environ['port']}"
 
     def GetMetrixNames(url):
         response = requests.get("{0}/api/v1/label/__name__/values".format(url))
         names = response.json()["data"]  # Return metrix names
         return names
-
-    """
-    Prometheus hourly data as csv.
-    """
 
     if len(sys.argv) != 1:
         print(
@@ -29,7 +97,6 @@ if __name__ == "__main__":
     metrixNames = GetMetrixNames(URL)
 
     today = datetime.today()
-
     today = today.strftime("%Y-%m-%d-%H-%M")
     prefix_dir = "metrics-arks"
     prefix_filename = os.getenv("DEPLOYMENT_NAME")
@@ -39,62 +106,31 @@ if __name__ == "__main__":
         os.mkdir(prefix_dir)
     except FileExistsError:
         pass
-    archive = f"{prefix_dir}/metrics_{prefix_filename}_{today}.tar.gz"
+    archive = f"{prefix_dir}/metrics_{prefix_filename}_{today}.tar.xz"
 
-    with tarfile.open(archive, "w:gz") as tar:
-        names = aliases()
-        with BytesIO() as tmpfile:
-            codecinfo = codecs.lookup("utf8")
-            file = codecs.StreamReaderWriter(
-                tmpfile, codecinfo.streamreader, codecinfo.streamwriter
-            )
-            writer = csv.writer(file, delimiter="\t")
-            writer.writerow(["instance", "name"])
-            for (key, value) in names.items():
-                writer.writerow([key, value])
-            tarinfo = tarfile.TarInfo("names.csv")
-            tarinfo.size = tmpfile.tell()
-            tmpfile.seek(0)
-            tar.addfile(tarinfo, tmpfile)
+    manager = mp.Manager()
+    queue = manager.Queue()
+    pool = mp.Pool(mp.cpu_count() + 2)
 
-        for metrixName in metrixNames:
-            with BytesIO() as tmpfile:
-                # Create a wrapper around the in-memory file in order to encode to utf-8 strings instead of bytes
-                codecinfo = codecs.lookup("utf8")
-                file = codecs.StreamReaderWriter(
-                    tmpfile, codecinfo.streamreader, codecinfo.streamwriter
-                )
-                writer = csv.writer(file, delimiter="\t")
+    # put listener to work first
+    pool.apply_async(listener, (queue, archive))
+    pool.apply_async(names, (queue,))
 
-                # now its hardcoded for hourly
-                response = requests.get(
-                    "{0}/api/v1/query".format(URL),
-                    params={"query": metrixName + f'[{os.environ["period"]}]'},
-                )
-                results = response.json()["data"]["result"]
-                # Build a list of all labelnames used.
-                # gets all keys and discard __name__
-                labelnames = set()
-                for result in results:
-                    labelnames.update(result["metric"].keys())
-                # Canonicalize
-                labelnames.discard("__name__")
-                labelnames = sorted(labelnames)
+    jobs = []
+    for metrixName in metrixNames:
+        job = pool.apply_async(worker, (queue, URL, metrixName))
+        jobs.append(job)
 
-                writer.writerow(["name"] + labelnames + ["timestamp", "value"])
-                for result in results:
-                    for label in labelnames:
-                        l = list(result["metric"].values())
-                        for value in result["values"]:
-                            writer.writerow(l + value)
+    # collect results from the workers through the pool result queue
+    for job in jobs:
+        job.get()
 
-                tarinfo = tarfile.TarInfo(f"{metrixName}.csv")
-                tarinfo.size = tmpfile.tell()
-                tmpfile.seek(0)
-                tar.addfile(tarinfo, tmpfile)
+    queue.put("kill")
+    pool.close()
+    pool.join()
 
     try:
-        os.remove("latest_metrics.tar.gz")
+        os.remove("latest_metrics.tar.xz")
     except (FileExistsError, FileNotFoundError):
         pass
-    os.symlink(archive, "latest_metrics.tar.gz")
+    os.symlink(archive, "latest_metrics.tar.xz")
