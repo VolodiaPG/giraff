a32777c7d942df9072971de3ecf0565aa7a8ad8e
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-04-26 16:07:30 +0200

    chore: update log utility

--- testbed/utils.sh
+++ testbed/utils.sh
@@ -1,3 +1,7 @@
+echodate(){
+    echo `date +%Y-%m-%d_%H:%M:%S`
+}
+
 log(){
   set +em
   text=$1
@@ -12,33 +16,43 @@ log(){
   shift # Remove arg
   shift # Remove arg
 
-  file=$(mktemp)
+  cmd=( "$@" )
 
-  {
-    $@ > $file 2>&1
-  }&
+  file=$(mktemp)
+  status_code_file=$(mktemp)
+
+
+  do_command() {
+    ${cmd[@]} > $file 2>&1
+    local status=$?
+    echo $status > $status_code_file
+  }
+  do_command &
   cmd_pid=$!
   # Wait for seconds and check if the command is still running
-  {
+  do_starting_block() {
+      timestamp=$(echodate)
       sleep 2
       if kill -0 $cmd_pid 2>/dev/null; then
-          echo -e "$text \033[33m...starting\033[0m"
+        echo -e "\033[37m$timestamp\033[0m $text \033[33m...starting\033[0m"
           wait $cmd_pid 2>/dev/null # Optionally wait for the command to actually finish
       else
           wait $cmd_pid 2>/dev/null # Command finished within 2 seconds
       fi
-  }&
+  }
+
+  do_starting_block &
   wait $cmd_pid
-  status=$?
+  status=$(cat $status_code_file)
   if [ $status -eq 0 ]; then
-    echo -e "$text \033[32mOK\033[0m"
+    echo -e "\033[37m$(echodate)\033[0m $text \033[32mOK\033[0m"
     rm $file
   else
-    echo -e "$text \033[31mFAILED\033[0m"
+    echo -e "\033[37m$(echodate)\033[0m $text \033[31mFAILED\033[0m"
     echo -en "\033[31mErr is: \033[0m"
     cat $file
     rm $file
-    exit $status
   fi
+  return $status
 }
 
