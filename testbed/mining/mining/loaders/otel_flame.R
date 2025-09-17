6894e6a5429da7a0328b0acbca5f75556b233197
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-09-17 09:16:38 +0200

    chore: remove leftover copy

--- testbed/mining/mining/loaders/otel_flame.R
+++
@@ -1,5 +0,0 @@
-flame_functions <- function(spans) {
-  spans %>%
-    filter(startsWith(span.name, "FLAME") & endsWith(span.name, "...")) %>%
-    mutate(span.name = substring(span.name, 1, nchar(span.name) - 3))
-}
